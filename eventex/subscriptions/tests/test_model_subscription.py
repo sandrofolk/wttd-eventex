68258edc48e3f4f72cec6a35c295de41d36590b9
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2016-09-30 09:53:48 -0300

    Adiciona campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -24,3 +24,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Alessandro de Lima Folk', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """By default paid must be False"""
+        self.assertEqual(False, self.obj.paid)
