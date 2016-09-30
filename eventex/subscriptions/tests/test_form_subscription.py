3575dfe732fb815b3f98a957f46ae2744798a3fb
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2016-09-30 11:40:25 -0300

    Valida CPF

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -4,10 +4,38 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
         """Form must have 4 fields."""
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertSequenceEqual(expected, list(self.form.fields))
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only accept digits."""
+        form = self.make_validated_form(cpf='ABCD5678901')
+        # self.assertFormErrorMessage(form, 'cpf', 'CPF deve conter apenas números.')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits."""
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def assertFormErrorMessage(self, form, field, msg):
+        errors = form.errors
+        errors_list = errors[field]
+        self.assertListEqual([msg], errors_list)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Alessandro de Lima Folk', cpf='12345678901',
+                    email='sandrofolk@hotmail.com', phone='43-9985-1513')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
