5bf65dc998f76d976d443749bf837d10ecc48f04
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2016-10-05 11:35:02 -0300

    Speaker

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,3 +1,22 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from eventex.core.models import Speaker
+
+
+class SpeakerModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'photo_link', 'website_link']
+
+    def website_link(self, obj):
+        return '<a href="{0}">{0}</a>'.format(obj.website)
+
+    website_link.allow_tags = True
+    website_link.short_description = 'website'
+
+    def photo_link(self, obj):
+        return '<img width="32px" src="{}" />'.format(obj.photo)
+
+    photo_link.allow_tags = True
+    photo_link.short_description = 'foto'
+
+admin.site.register(Speaker, SpeakerModelAdmin)
--- eventex/urls.py
+++ eventex/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from eventex.core import views
+from eventex.core.views import home, speaker_detail
 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', home, name='home'),
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', admin.site.urls),
 ]
